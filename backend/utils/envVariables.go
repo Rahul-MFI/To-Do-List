750fd762c32bdcac8646c6b13ebd81f51dbfeb23
VRAHUL1232 <rahulv210804@gmail.com>
2025-10-06 19:04:12 +0530

    fix: made port default value

--- backend/utils/envVariables.go
+++ backend/utils/envVariables.go
@@ -21,6 +21,9 @@ func LoadEnv(file string) {
 	}
 	env.DB_HOST = os.Getenv("DB_HOST")
 	env.DB_PORT = os.Getenv("DB_PORT")
+	if env.DB_PORT == "" || env.DB_PORT == "0" {
+		env.DB_PORT = "3306"
+	}
 	env.DB_USER = os.Getenv("DB_USER")
 	env.DB_PASS = os.Getenv("DB_PASS")
 	env.DB_NAME = os.Getenv("DB_NAME")
