90059dd776342b3b51af45de24d1c6cde705b5f8
VRAHUL1232 <rahulv210804@gmail.com>
2025-10-08 10:34:36 +0530

    fix: made changes in NetworkProvider page

--- frontend/src/components/NetworkProvider.jsx
+++ frontend/src/components/NetworkProvider.jsx
@@ -7,25 +7,6 @@ const NetworkProvider = ({ children }) => {
   const [session, setSession] = useState(true);
   const [soundEnabled, setSoundEnabled] = useState(true);
 
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.ready.then(() => {
-
-        const handleMessage = (event) => {
-          if (event.data?.type === "push-sound") {
-              const audio = new Audio("/audio.mp3");
-              audio.play().catch((err) => console.warn("Audio blocked:", err));
-          }
-        };
-
-        navigator.serviceWorker.addEventListener("message", handleMessage);
-        return () => {
-          navigator.serviceWorker.removeEventListener("message", handleMessage);
-        };
-      });
-    }
-  }, []);
-
   return (
     <NetworkContext.Provider value={{ online, setOnline, session, setSession, soundEnabled, setSoundEnabled }}>
       {children}
