7aa124f86c0fa705fa3f20297780a9a1be1046e5
VRAHUL1232 <rahulv210804@gmail.com>
2025-10-21 02:39:08 +0530

    fix: Cors error

--- backend/routes/router.go
+++ backend/routes/router.go
@@ -10,7 +10,7 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://todo-dun-three.vercel.app"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://to-do-list-rust-five-60.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
