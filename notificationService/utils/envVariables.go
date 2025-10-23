a9ce2c92df08594bb828f0d354a7da867149fac4
VRAHUL1232 <rahulv210804@gmail.com>
2025-10-24 01:30:35 +0530

    fix: .env probelm in cloud

--- notificationService/utils/envVariables.go
+++ notificationService/utils/envVariables.go
@@ -13,20 +13,22 @@ var env *model.Env
 // LoadEnv loads env vars from a given file
 func LoadEnv(file string) {
 	env = &model.Env{}
-	err := godotenv.Load(file)
-	if err != nil {
-		log.Fatalf("Error loading %s file: %v", file, err)
+	if _, err := os.Stat(".env"); err == nil {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Println("Warning: Failed to load .env file:", err)
+		}
 	}
 	env.DB_HOST = os.Getenv("DB_HOST")
 	env.DB_PORT = os.Getenv("DB_PORT")
+	if env.DB_PORT == "" || env.DB_PORT == "0" {
+		env.DB_PORT = "3306"
+	}
 	env.DB_USER = os.Getenv("DB_USER")
 	env.DB_PASS = os.Getenv("DB_PASS")
 	env.DB_NAME = os.Getenv("DB_NAME")
 	env.JWT_SECRET = os.Getenv("JWT_SECRET")
 	env.JWT_EXPIRATION = os.Getenv("JWT_EXPIRATION")
-	env.VAPID_PUBLIC_KEY = os.Getenv("VAPID_PUBLIC_KEY")
-	env.VAPID_PRIVATE_KEY = os.Getenv("VAPID_PRIVATE_KEY")
-
 }
 
 // GetEnv returns the loaded env
